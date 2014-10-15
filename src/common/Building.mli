58d52eaf53a611095b1b0b9a91e0f97d106e15cd
Mathias Sable-meyer <sable-meyer@crans.org>
2014-10-15 18:19:28 +0200

    Added interface for Buildings

--- src/common/Building.mli
+++ src/common/Building.mli
@@ -4,3 +4,9 @@ type t
 
 val texture_name : t -> string
 
+(* List of Units a Building is able to produce *)
+val get_producible : t -> Unit.t list
+
+(* Income of a given building *)
+val get_income : t -> Resource.t
+
