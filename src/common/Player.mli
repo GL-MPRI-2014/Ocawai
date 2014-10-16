db167926267f0add665491839c24dd5a597b9adf
VLanvin <victor.lanvin@ens-cachan.fr>
2014-10-16 10:35:42 +0200

    Added setters to Player.mli

--- src/common/Player.mli
+++ src/common/Player.mli
@@ -2,6 +2,10 @@ type t
 
 val get_army : t -> Unit.t list
 
+val add_unit : t -> Unit.t -> unit
+
 val get_buildings : t -> Building.t list
+
+val add_building : t -> Building.t -> unit
 
 val get_next_action : t -> (Action.movement * Action.action)
