1c09cad07c1f66a40f61545a26f2b8052b875edb
VLanvin <victor.lanvin@ens-cachan.fr>
2014-10-16 10:12:53 +0200

    Added actions

---
+++ src/common/Action.mli
@@ -0,0 +1,7 @@
+type movement = Position.t list
+
+type action = 
+  Attack_unit of (Unit.t * Unit.t) | 
+  Attack_building of (Unit.t * Building.t) |
+  Wait
+
--- src/common/Player.mli
+++ src/common/Player.mli
@@ -3,3 +3,5 @@ type t
 val get_army : t -> Unit.t list
 
 val get_buildings : t -> Building.t list
+
+val get_next_action : t -> (Action.movement * Action.action)
