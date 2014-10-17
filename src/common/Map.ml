12f3a4a0a96ffc1367f93e4a8b73db43bd1ecd8e
Sheeft <isheeft@gmail.com>
2014-10-17 16:39:25 +0200

    First implementation of Map (for testing)
    It might serve as a base for the actual Map but it hasn't been
    made for that purpose. It is essentially to begin the tests.

---
+++ src/common/Map.ml
@@ -0,0 +1,18 @@
+(** This file is not meant to be pushed on master.                           **)
+(** It is only built for testing purpose while the real one is not ready.    **)
+
+type t = Tile.t array array
+
+let get_tile map pos =
+  let (x,y) = Position.topair pos in
+  map.(x).(y)
+
+let set_tile map pos tile =
+  let (x,y) = Position.topair pos in
+  map.(x).(y) <- tile
+
+let tile_iter f map =
+  Array.iter (Array.iter f) map
+
+let tile_iteri f map =
+  Array.iteri (fun x -> Array.iteri (fun y -> f (Position.create x y))) map
