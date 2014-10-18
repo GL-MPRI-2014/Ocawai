eac99a191a494002b445f058278a9559da0547d2
Sheeft <isheeft@gmail.com>
2014-10-19 00:05:02 +0200

    Typo (forest instead of water)
    Be warned though! `Tile.create_from_file` won't be used that way
    in an uncertain future!

--- src/engine/FieldGenerator.ml
+++ src/engine/FieldGenerator.ml
@@ -1,9 +1,8 @@
 
 let generate width height =
-  let tiles = [("water", Tile.create_from_file "forest" "");
+  let tiles = [("water", Tile.create_from_file "water" "");
     ("forest", Tile.create_from_file "forest" "");
     ("plain", Tile.create_from_file "plain" "");
     ("concrete", Tile.create_from_file "concrete" "")] in
   let map = Battlefield.create width height (List.assoc "water" tiles) in
   map
-
