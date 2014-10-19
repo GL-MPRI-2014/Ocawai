9a7533be2103cee1f9c6f398b2bcf1bee604ffb6
Sheeft <isheeft@gmail.com>
2014-10-19 16:59:17 +0200

    Even better when consistent between files

--- src/common/Unit.ml
+++ src/common/Unit.ml
@@ -2,7 +2,7 @@
 
 type t = {name : string; pos : Position.t}
 
-type movement = Walk | Swim | Fly | Amphibious
+type movement = Walking | Swimming | Flying | Amphibious
 
 let get_name t = t.name
 
@@ -17,6 +17,6 @@ let attack_range t = 2
 let move_range t = 4
 
 (* Wow. Much bad. *)
-let create_from_file s1 s2 = 
-  {name = "infantry"; 
+let create_from_file s1 s2 =
+  {name = "infantry";
    pos = Position.create (int_of_string s1, int_of_string s2)}
