1a84fdb3c78035246cfc44e0f76e2eb9bd68f320
Mazzocchi <nicolas.mazzocchi@live.fr>
2014-10-19 15:48:28 +0200

    Serveur coucou

--- src/Reseaux/serveur.ml
+++ src/Reseaux/serveur.ml
@@ -1,1 +1,14 @@
-(* commentaire *)
+#load "Unix.cma"
+open Unix
+
+let _ =
+  let port = 1024 in
+  let listen_sock = Unix.socket PF_INET SOCK_STREAM 0 in
+  bind listen_sock (ADDR_INET  (inet_addr_any,port)) ;
+  listen listen_sock 16 ;
+  let (sock,_) = accept listen_sock in
+  close listen_sock ;
+  let count = send sock "coucou" 0 (String.length "coucou") [] in
+  close sock ;
+  count
+
