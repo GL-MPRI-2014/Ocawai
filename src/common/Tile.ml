466092f64034d998df582756d00f175e78d337a3
François Thiré <franth2@gmail.com>
2014-10-22 09:52:08 +0200

    Add new stuff on Tile.ml

--- src/common/Tile.ml
+++ src/common/Tile.ml
@@ -1,13 +1,21 @@
 (* Dummy Tile implementation *)
 
-type t = string
+type tile_t = 
+  {
+    name : string
+    walkable : bool;
+    navigable : bool;
+    flyable : bool;
+    cost : int;
+  }
+type t = tile_t
 
-let get_name tile = tile
+let get_name tile = tile.string
 
-let walkable tile = true
-let navigable tile = true
-let flyable tile = true
+let walkable tile = tile.walkable
+let navigable tile = tile.navigable
+let flyable tile = tile.flyable
 
-let movement_cost tile mouvement = 1
+let movement_cost tile mouvement = tile.cost
 (* Awful *)
 let create_from_file s1 s2 = s1
