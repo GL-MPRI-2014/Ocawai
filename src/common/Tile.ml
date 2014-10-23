dba7a45a14a223e5d9497a7b3878339faef3b3e1
dbusatto <damien.busatto@gmail.com>
2014-10-23 12:38:39 +0200

    separate rol and walk costs

--- src/common/Tile.ml
+++ src/common/Tile.ml
@@ -16,7 +16,7 @@ let walkable tile = tile.walkable
 let navigable tile = tile.navigable
 let flyable tile = tile.flyable
 
-let movement_cost tile mouvement = tile.cost
+let movement_cost tile movement = tile.cost
 
 (* Awful *)
 let create_from_file s1 s2 = {name = s1; walkable = true; navigable = false; flyable = false; cost = 1 }
