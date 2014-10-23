492fa0534b11ad3129a241154825992380d3b0c6
Artymort <m.berta@laposte.net>
2014-10-23 12:40:04 +0200

    First interface for DList.
    This is the backbone of the module Tile.

---
+++ src/music/DList.mli
@@ -0,0 +1,53 @@
+(**
+ * Doubly chained list module used in music generation.
+ * implements constant time insert in head AND tail.
+ * From [Théis]' internship.
+**)
+
+type time
+type event
+type t
+
+(**
+ * Delay to sync various tiles
+**)
+val sync : time -> t
+
+(**
+ * Neutral element for the Tiled product
+**)
+val zero : t
+
+(**
+ * Compares t to zero
+**)
+val isZero : t -> bool
+
+(**
+ * Encapsulate an event into a singleton, that is a single element list
+**)
+val return : event -> t
+
+(**
+ * Infix operation for concatenation
+ *
+ * Eventually this infix operator should be modified, we wanted ::: but somehow
+ * this is not possible ...
+**)
+val (/::/) : t -> t -> t
+
+(**
+ * Split the abstract tree to its head and tail with respect to the time
+**)
+val headTail : t -> t * t
+
+(**
+ * For testing purpose
+**)
+val fromList : event list -> t
+
+(**
+ * Idem
+**)
+val toList : t -> event list
+
