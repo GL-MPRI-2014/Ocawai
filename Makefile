b3af068296119603ffddd3755c15f0c698fe8c2a
Olivier Marty <olivier.marty.m@gmail.com>
2014-10-24 12:34:16 +0200

    Use atdgen to parse tiles.json First example of using atdgen to parse a config file

--- Makefile
+++ Makefile
@@ -1,13 +1,28 @@
 INTERFACE_SRC = src/common,src/interface
 ENGINE_SRC = src/engine,src/common
-PACKAGES = ocsfml.graphics
+PACKAGES = ocsfml.graphics,atdgen
 
 OUTPUT = main.native
 
-interface:
+find_files_atd = $(wildcard $(dir)/*.atd)
+
+dirs := src/common src/interface
+files_atd := $(foreach dir,$(dirs),$(find_files_atd))
+files_atd_ml := $(files_atd:.atd=_t.ml) $(files_atd:.atd=_j.ml)
+files_atd_mli := $(files_atd_ml:.ml=.mli)
+
+default: interface
+
+# atd files
+%_t.ml: %.atd
+	atdgen -t $<
+%_j.ml: %.atd
+	atdgen -j $<
+
+interface: $(files_atd_ml)
 	ocamlbuild -use-ocamlfind -Is $(INTERFACE_SRC) -package $(PACKAGES) $(OUTPUT)
 
-engine :
+engine : $(files_atd_ml)
 	ocamlbuild -use-ocamlfind -Is $(ENGINE_SRC) -package $(PACKAGES) $(OUTPUT)
 
 run: interface
@@ -15,3 +30,4 @@ run: interface
 
 clean:
 	ocamlbuild -clean
+	rm -f $(files_atd_ml) $(files_atd_mli)
--- src/common/Tile.ml
+++ src/common/Tile.ml
@@ -1,41 +1,34 @@
 (* Dummy Tile implementation *)
 
-type tile_t = 
-  {
-    name : string;
-    walk_cost : int;
-    wheels_cost : int;
-    tracks_cost : int;
-    swim_cost : int;
-    fly_cost : int
-  }
+type tile_t = Tile_t.t
 type t = tile_t
 
-let get_name tile = tile.name
+let get_name tile = tile.Tile_t.name
 
-let walkable tile = tile.walk_cost >= 0
-let rollable tile = tile.wheels_cost >= 0 (*ouch*)
-let trackable tile = tile.tracks_cost >= 0 (*big ouch*)
-let navigable tile = tile.swim_cost >= 0
-let flyable tile = tile.fly_cost >= 0
+let walkable tile = tile.Tile_t.walk_cost >= 0
+let rollable tile = tile.Tile_t.wheels_cost >= 0 (*ouch*)
+let trackable tile = tile.Tile_t.tracks_cost >= 0 (*big ouch*)
+let navigable tile = tile.Tile_t.swim_cost >= 0
+let flyable tile = tile.Tile_t.fly_cost >= 0
 
 let movement_cost tile movement = 
 let min_pos a b = if a >=0 then if b>=0 then min a b else a else b in
 let cost = begin 
     let open Unit in
     match movement with
-    | Walk -> tile.walk_cost
-    | Roll -> tile.wheels_cost
-    | Tracks -> tile.tracks_cost
-    | Swim -> tile.swim_cost
-    | Fly -> tile.fly_cost
-    | Amphibious_Walk -> min_pos tile.swim_cost tile.walk_cost
-    | Amphibious_Roll -> min_pos tile.swim_cost tile.wheels_cost
-    | Amphibious_Tracks-> min_pos tile.swim_cost tile.tracks_cost
+    | Walk -> tile.Tile_t.walk_cost
+    | Roll -> tile.Tile_t.wheels_cost
+    | Tracks -> tile.Tile_t.tracks_cost
+    | Swim -> tile.Tile_t.swim_cost
+    | Fly -> tile.Tile_t.fly_cost
+    | Amphibious_Walk -> min_pos tile.Tile_t.swim_cost tile.Tile_t.walk_cost
+    | Amphibious_Roll -> min_pos tile.Tile_t.swim_cost tile.Tile_t.wheels_cost
+    | Amphibious_Tracks-> min_pos tile.Tile_t.swim_cost tile.Tile_t.tracks_cost
 end in
 if cost >=0 then cost else failwith("Tile.movement_cost : not a valid movement")
 
-(* Awful *)
-let create_from_file s1 s2 = {name = s1; walk_cost = 2; wheels_cost = 2;tracks_cost = 2; swim_cost = -1; fly_cost = 1}
-
-
+let create_from_file s1 s2 =
+  List.find
+    (fun tile -> tile.Tile_t.name = s1)
+    (Ag_util.Json.from_file Tile_j.read_t_list "resources/config/tiles.json"(*s2*))
+  (*TODO : cache *)
