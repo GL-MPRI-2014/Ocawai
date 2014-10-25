4056078317ccb05fe5155830724b9ead68e955f8
Mazzocchi <nicolas.mazzocchi@live.fr>
2014-10-25 17:01:46 +0200

    Serveur a scrutation et defragmentation
    Les messages sont defragmentes par le serveur.
    La scrutation est a present en lecture et en ecriture.
    Une fonction gere la permission en l'ecriture.
    Pour l'instant le serveur retourne la string envoye par
    le client. Attention, le parse ne se fait pas avec les
    '\n' mais avec des #. Par exemple
    Client -> toto#titi#tutu\n
    Serveur -> Bien recu : "toto"
    Serveur -> Bien recu : "titi"
    Client -> #
    Serveur -> Bien recu : "tutu
    "

--- src/Reseaux/serveur.ml
+++ src/Reseaux/serveur.ml
@@ -1,46 +1,135 @@
-#load "Unix.cma"
-open Unix
+let port =
+  if Array.length Sys.argv = 2 then
+    int_of_string Sys.argv.(1)
+  else 
+    failwith "not specified port"
 
-let _ =
-  let port = 1024 in
-  let listen_sock = Unix.socket PF_INET SOCK_STREAM 0 in
-  bind listen_sock (ADDR_INET  (inet_addr_any,port)) ;
-  listen listen_sock 16 ;
-  let (sock,_) = accept listen_sock in
-  close listen_sock ;
-  let count = send sock "coucou" 0 (String.length "coucou") [] in
-  close sock ;
-  count
+let fd_listen = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0
+let () =
+  try
+    Unix.setsockopt fd_listen Unix.SO_REUSEADDR true;
+    Unix.bind fd_listen (Unix.ADDR_INET (Unix.inet_addr_any, port));
+    Unix.listen fd_listen 16;
+    Unix.set_nonblock fd_listen
+  with
+    | Unix.Unix_error (Unix.EADDRINUSE, _, _) ->
+        failwith "port already in use"
+      
+module FDSet = Set.Make
+  (struct
+    type t = Unix.file_descr
+    let compare = compare
+   end
+  )
+  
+let clients = ref (FDSet.singleton fd_listen)
+  
+let input_buffer = Hashtbl.create 0
+let output_buffer = Hashtbl.create 0
+let flush_buffer = Hashtbl.create 0
+  
+let buffer_size = 1024
+let buffer = String.make buffer_size ' '
+  
+let handle client requests =
+  List.iter
+    (fun request ->
+      let data = Printf.sprintf "Bien recu : \"%s\"\n" request in
+      Printf.fprintf stdout "%s" request ;
+      flush stdout ;
+      Hashtbl.replace output_buffer client
+	(try Hashtbl.find output_buffer client ^ data
+         with Not_found -> data
+	);
+    ) requests
 
-
-
-
-module FDSet = Set.Make struct
-  type t = file_descr
-  let compare = compare
-end
-
-let clients = ref (FDSet.singleton listen_sock)
-
-let _ =
+let () =
   while true do
-    let (fd_read, _, _) = select (FDSet.elements !clients) [] [] -1 in
-    List.iter (fun client ->
-      begin
-	if client = listen_sock then
-	  let (client, _) = accept listen_sock in
-	  clients := FDSet.add client !clients
-	else
-	  begin
-	    let count = read client buffer 0 buffer_size) in
-            if count = 0 then
-	      begin
-		clients := FDSet.remove client !clients ;
-		close client
-	      end
-	    else
-	      Printf.printf "%s\n" buffer
+    let (fd_read, _, _) =
+      Unix.select (FDSet.elements !clients) [] [] 1.0 in
+    List.iter
+      (fun client ->
+        if client = fd_listen then
+          begin
+            let (client, addr) = Unix.accept fd_listen in
+            clients := FDSet.add client !clients;
+            Unix.set_nonblock client
           end
-      end
-    ) fd_read
-  done 
+        else
+          begin
+            let read_size =
+              try
+                Some (Unix.read client buffer 0 buffer_size)
+              with Unix.Unix_error (_, _, _) ->
+                None in
+            match read_size with
+              | None | Some 0 ->
+                Hashtbl.remove input_buffer client;
+                Hashtbl.remove output_buffer client;
+                Hashtbl.remove flush_buffer client;
+                
+                clients := FDSet.remove client !clients;
+                Unix.close client
+
+              | Some read_size ->
+                let data = String.sub buffer 0 read_size in
+                Hashtbl.replace input_buffer client
+                  (try Hashtbl.find input_buffer client ^ data
+                   with Not_found -> data);
+                try
+                  while true do
+                    let data = Hashtbl.find input_buffer client in
+                    let index = String.index data '#' in
+                    Hashtbl.replace input_buffer client
+		      (String.sub data
+			 (index + 1)
+                         (String.length data - index - 1)
+		      );
+                    Hashtbl.replace flush_buffer client
+		      ((try Hashtbl.find flush_buffer client
+			with Not_found -> []
+		       ) @ [String.sub data 0 index]
+		      )
+                  done
+                with Not_found -> ()
+          end
+      ) fd_read;
+    
+    Hashtbl.iter handle flush_buffer;
+    Hashtbl.clear flush_buffer;
+    
+    let (_, fd_write, _) =
+      Unix.select [] (FDSet.elements !clients) [] 1.0 in
+    let fd_write =
+      List.filter (Hashtbl.mem output_buffer) fd_write in
+    List.iter
+      (fun client ->
+        let data = Hashtbl.find output_buffer client in
+        let write_size =
+          try
+            Some (Unix.single_write client data 0 (String.length data))
+          with
+            | Unix.Unix_error (Unix.EAGAIN, _, _) ->
+              Some 0
+            | Unix.Unix_error (_, _, _) ->
+              None in
+	
+        match write_size with
+          | None ->
+            Hashtbl.remove input_buffer client;
+            Hashtbl.remove output_buffer client;
+            Hashtbl.remove flush_buffer client;
+	    
+            clients := FDSet.remove client !clients;
+            Unix.close client
+          | Some write_size ->
+            if write_size = String.length data then
+	      Hashtbl.remove output_buffer client
+            else
+	      Hashtbl.replace output_buffer client
+		(String.sub data
+		   write_size
+                   (String.length data - write_size)
+		)
+      ) fd_write;
+  done
