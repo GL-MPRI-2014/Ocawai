04b653089574ba5d1f3028e8ce637a13499db03c
MetaMetaMath <pichu83@hotmail.fr>
2014-10-27 16:36:48 +0100

    Added accessible_positions (not tested)

--- src/common/Logics.mli
+++ src/common/Logics.mli
@@ -1,7 +1,17 @@
 (** Some useful functions related to the game's logic *)
 
-(** Return the list of positions that a given unit can see *)
+(** Returns true iff the given position is within the battlefield *)
+val in_battlefield : Battlefield.t -> Position.t -> bool
+
+(** Returns the list of positions that a given unit can see *)
 val unit_vision : Unit.t -> Battlefield.t -> Position.t list
 
-(** Return the list of all positions that a given player can see *)
+(** Returns the list of all positions that a given player can see *)
 val player_vision : Player.t -> Battlefield.t -> Position.t list
+
+(** Returns the cost of a path for a given movement type *)
+val path_movement_cost : Unit.movement -> Battlefield.t -> Action.movement -> int
+
+(** For a given unit, returns the hashtable where the keys are the
+    accessible positions and the entry is a shortest path to this position *)
+val accessible_positions : Unit.t -> Player.t -> Player.t list -> Battlefield.t -> (Position.t, Action.movement) Hashtbl.t
