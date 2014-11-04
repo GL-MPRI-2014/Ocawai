42e8940da994200058f48df316c914b659939bc0
Sheeft <isheeft@gmail.com>
2014-11-04 23:31:47 +0100

    Added screen to Home (interface)

--- src/interface/gui/Home.mli
+++ src/interface/gui/Home.mli
@@ -15,3 +15,11 @@ class actionnable : (unit -> unit) -> object
   method action : unit
 
 end
+
+(** A screen (simply a menu)
+  * It has the particularity it handles selection with position *)
+class screen : item list -> actionnable list -> object
+
+  method draw : OcsfmlGraphics.render_target -> unit
+
+end
