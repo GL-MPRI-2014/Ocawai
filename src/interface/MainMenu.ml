80aed0234d9a10a508654a9bc552727bc3a96f6d
Sheeft <isheeft@gmail.com>
2014-11-05 00:29:01 +0100

    Now handling resizing for mainmenu

--- src/interface/MainMenu.ml
+++ src/interface/MainMenu.ml
@@ -24,10 +24,26 @@ class main_menu = object(self)
   method private set_splash_size s =
     splash_size <- s
 
+  method private set_screen w h =
+    let (w,h) = foi2D (w,h) in
+    screen <- new Home.screen
+      [new Home.item "title" (w/.2., h /. 2. -. 250.)]
+      [
+        new Home.actionnable "gameon" "gameon_hover" (w/.2., h /. 2. +. 30.)
+          (fun () -> Printf.printf "gameon!\n") ;
+        new Home.actionnable "quit" "quit_hover"
+          (w /. 2. -. 130., h /. 2. +. 230.)
+          (fun () -> Printf.printf "quit\n") ;
+        new Home.actionnable "settings" "settings_hover"
+          (w /. 2. +. 100., h /.2. +. 220.)
+          (fun () -> Printf.printf "settings\n")
+      ]
+
   method handle_event e =
 
     OcsfmlWindow.Event.(
       match e with
+        | Resized { width = w ; height = h } -> self#set_screen w h
         | KeyPressed { code = _ ; _ } ->
             (new Game.game :> State.state) |> manager#push
         | _ -> ()
@@ -56,20 +72,8 @@ class main_menu = object(self)
       self#set_alpha 2. 0.4 0.6);
     ignore(Interpolators.new_sine_ip
       self#set_splash_size 1.8 0.05 1.) *)
-    (* TODO handle resizing with that too ! *)
     let window = manager#window in
-    let (w,h) = foi2D window#get_size in
-    screen <- new Home.screen
-      [new Home.item "title" (w/.2., h /. 2. -. 250.)]
-      [
-        new Home.actionnable "gameon" "gameon_hover" (w/.2., h /. 2. +. 30.)
-          (fun () -> Printf.printf "gameon!\n") ;
-        new Home.actionnable "quit" "quit_hover"
-          (w /. 2. -. 130., h /. 2. +. 230.)
-          (fun () -> Printf.printf "quit\n") ;
-        new Home.actionnable "settings" "settings_hover"
-          (w /. 2. +. 100., h /.2. +. 220.)
-          (fun () -> Printf.printf "settings\n")
-      ]
+    let (w,h) = window#get_size in
+    self#set_screen w h
 
 end
