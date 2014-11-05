fa2f2fec3a94e14c071919e4e5176058d8d8b285
Paul Gallot <paul.gallot@ens-cachan.fr>
2014-11-05 15:00:57 +0100

    ajout de set_army et create_player

--- src/common/Player.mli
+++ src/common/Player.mli
@@ -1,9 +1,11 @@
 
 type t = <
   get_army : Unit.t list;
+  set_army : Unit.t list -> unit;
   add_unit : Unit.t -> unit;
   get_buildings : Building.t list;
   add_building : Building.t -> unit;
   get_next_action : Action.t
 >
 
+val create_player : t
