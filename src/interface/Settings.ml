983779965f574da3d7a4fbfa3c544eb842f79749
Sheeft <isheeft@gmail.com>
2014-11-10 23:37:23 +0100

    Use of settings in Camera

--- src/interface/Settings.ml
+++ src/interface/Settings.ml
@@ -3,14 +3,14 @@ type screen_mode = Fullscreen | Windowed
 let settings = object(self)
 
   val mutable mode = Fullscreen
-  val mutable move_speed = 20.
+  val mutable cursor_speed = 20.
   val mutable zoom_speed = 10.
 
   method set_mode v = mode <- v
   method mode = mode
 
-  method set_move_speed v = move_speed <- v
-  method move_speed = move_speed
+  method set_cursor_speed v = cursor_speed <- v
+  method cursor_speed = cursor_speed
 
   method set_zoom_speed v = zoom_speed <- v
   method zoom_speed = zoom_speed
--- src/interface/Settings.mli
+++ src/interface/Settings.mli
@@ -6,12 +6,15 @@ type screen_mode = Fullscreen | Windowed
 (** Object to hold settings *)
 val settings : <
 
+  (** Sets the screen mode (TODO) *)
   set_mode : screen_mode -> unit ;
   mode : screen_mode ;
 
-  set_move_speed : float -> unit ;
-  move_speed : float ;
+  (** Speed of the cursor (in game) *)
+  set_cursor_speed : float -> unit ;
+  cursor_speed : float ;
 
+  (** Speed of the zoom *)
   set_zoom_speed : float -> unit ;
   zoom_speed : float
 
