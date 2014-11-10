4307a5b2344a6203d09928c59c29c6f700387ce1
Sheeft <isheeft@gmail.com>
2014-11-10 18:10:15 +0100

    Added back button

--- src/interface/SettingsScreen.ml
+++ src/interface/SettingsScreen.ml
@@ -8,12 +8,18 @@ class state = object(self)
 
   inherit State.state as super
 
+  val mutable screen = new Home.screen [] []
+
   val font = new font `None
 
-  val mutable text_alpha = 1.
-
-  method private set_alpha a =
-    text_alpha <- a
+  method private set_screen w h =
+    let (w,h) = foi2D (w,h) in
+    screen <- new Home.screen
+      []
+      [
+        new Home.actionnable "back" "back_hover" (w/.2., h /. 2. +. 30.)
+          (fun () -> manager#pop) ;
+      ]
 
   method handle_event e =
 
@@ -21,14 +27,22 @@ class state = object(self)
       match e with
         | KeyPressed { code = OcsfmlWindow.KeyCode.Back ; _ } ->
             manager#pop
+        | KeyPressed { code = OcsfmlWindow.KeyCode.Left ; _ } ->
+            screen#left
+        | KeyPressed { code = OcsfmlWindow.KeyCode.Right ; _ } ->
+            screen#right
+        | KeyPressed { code = OcsfmlWindow.KeyCode.Up ; _ } ->
+            screen#up
+        | KeyPressed { code = OcsfmlWindow.KeyCode.Down ; _ } ->
+            screen#down
+        | KeyPressed { code = OcsfmlWindow.KeyCode.Return ; _ } ->
+            screen#action
         | _ -> ()
     )
 
   method render window =
 
     super#render window ;
-
-    Interpolators.update ();
 
     let color = Color.rgb 221 224 234 in
     window#clear ~color ();
@@ -39,13 +53,16 @@ class state = object(self)
       window "SETTINGS" font Color.black (Pix 70) (Pix 10) Left
       { left = 10. ; top = 10. ; width = w -. 20. ; height = 100. };
 
+    screen#draw window;
+
     window#display
 
   initializer
     if not (font#load_from_file "resources/fonts/Roboto-Black.ttf")
     then failwith "Couldn't load the font here";
-    ignore(Interpolators.new_sine_ip
-      self#set_alpha 2. 0.4 0.6);
+    let window = manager#window in
+    let (w,h) = window#get_size in
+    self#set_screen w h
 
   method destroy =
     ()
