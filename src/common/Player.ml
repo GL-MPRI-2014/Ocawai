fc5250bea19582d551b89af43b06d82fd8111040
François Thiré <franth2@gmail.com>
2014-11-11 14:10:12 +0100

    Nothing for the moment.

--- src/common/Player.ml
+++ src/common/Player.ml
@@ -6,8 +6,10 @@ object (self)
   method set_army a = army <- a
 
   method add_unit u = army <- u::army
-
+                              
+  
   method get_buildings = buildings
+
   method add_building b = buildings <- b::buildings
   method set_buildings (buildings: Building.t list) =();
 
--- src/common/Player.mli
+++ src/common/Player.mli
@@ -1,20 +1,21 @@
 
 type t = <
+  (*TO DO: The type Unit.t list could (should) not be exposed. Instead,
+    we could create a module allowing some functions as search one unit,
+    get a list of unit etc... *)
   get_army : Unit.t list;
   set_army : Unit.t list -> unit;
 
   add_unit : Unit.t -> unit;
-
+  (*remove_unit : Unit.t -> unit;*)
+  
   get_buildings : Building.t list;
   set_buildings : Building.t list -> unit;
 
   add_building : Building.t -> unit;
-
+  (*remove_building : Building.t -> unit;*)
+  
   get_next_action : Action.t;    (* Appelé par la grosse boucle, renvoie depuis la file l'action que veut effectuer le joueur *)
-  push_next_action : Action.t -> unit; (* Appelé par l'interface, permet d'envoyer dans le file l'action que veut effectuer le joueur *)
-
-  last_game_update : Action.t; (* Appelé par l'interface, renvoie la dernière action effectuer sur le jeu par n'importe quelle player. *)
-  push_last_game_update : Action.t -> unit (* Appelé par la GB, envoie la dernière action effectuer *)
 >
 
 val create_player : unit -> t
