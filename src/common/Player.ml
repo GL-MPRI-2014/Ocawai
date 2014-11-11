012eb0d7ee1c685181f6d62ce191909778d9a03f
Charlie <jacomme@crans.org>
2014-11-11 12:11:14 +0100

    Revert "PLayer interface definitions"
    This reverts commit b1d22441ef16654a849643b78180a5370a1f2c8d.

--- src/common/Player.ml
+++ src/common/Player.ml
@@ -4,18 +4,10 @@ object (self)
   val mutable buildings = (b : Building.t list)
   method get_army = army
   method set_army a = army <- a
-
   method add_unit u = army <- u::army
-
   method get_buildings = buildings
   method add_building b = buildings <- b::buildings
-  method set_buildings (buildings: Building.t list) =();
-
   method get_next_action = ([]:Action.movement),Action.Wait
-  method push_next_action (action:Action.t) = ()
-
-  method last_game_update = ([]:Action.movement),Action.Wait
-  method push_last_game_update (action:Action.t) = ()
 end
 
 type t = player
