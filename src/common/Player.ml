6e1ec38cdea8452d0cc2891ab1a7f91bbe892036
François Thiré <franth2@gmail.com>
2014-11-12 11:11:37 +0100

    Implementation of dummy player + add method set_buildings

--- src/common/Player.ml
+++ src/common/Player.ml
@@ -1,25 +1,49 @@
-class virtual player (a : Unit.t list) (b : Building.t list) = 
-object (self)
-  val mutable army = (a : Unit.t list)
-  val mutable buildings = (b : Building.t list)
-  method get_army = army
-  method set_army a = army <- a
+open List
 
-  method add_unit u = army <- u::army
-                              
+class  player (a : Unit.t list) (b : Building.t list) =
+  object (self)
+    val mutable army = (a : Unit.t list)
+    val mutable buildings = (b : Building.t list)
+    method get_army = army
+    method set_army a = army <- a
+    method add_unit u = army <- u::army
+    method get_buildings = buildings
+    method add_building b = buildings <- b::buildings
+    method get_next_action = ([]:Action.movement),Action.Wait
+  end
   
+(*
+class virtual player (army_ : Unit.t list) (buildings_ : Building.t list) = 
+object (self)
+  val mutable army = (army_ : Unit.t list)
+  val mutable buildings = (buildings_ : Building.t list)
+                          
+  method get_army = army
+  method add_unit u = army <- u::army
+  method set_army army_ = army <- army_
+    
   method get_buildings = buildings
-
   method add_building b = buildings <- b::buildings
-  method set_buildings (buildings: Building.t list) =();
-
-  method get_next_action = ([]:Action.movement),Action.Wait
-  method push_next_action (action:Action.t) = ()
-
-  method last_game_update = ([]:Action.movement),Action.Wait
-  method push_last_game_update (action:Action.t) = ()
+  method set_buildings buildings_ = buildings <- buildings_
+    
+  method virtual get_next_action :  Action.t
 end
+*)
+
+
+class dummy_player army_ buildings_ (a: Action.t list) =
+  object
+    inherit player army_ buildings_
+    val mutable actions = (a: Action.t list)
+    method get_next_action  =
+      if length a == 0 then
+        ([Position.create (0,0)], Wait)
+      else
+        let action= hd(actions) in
+        actions<-tl(actions);
+        action
+  end
 
 type t = player
 
-let create_player () = new player [] [] 
+let create_player () = new dummy_player [] []  []
--- src/common/Player.mli
+++ src/common/Player.mli
@@ -1,37 +1,17 @@
-module type FAMILY =
-sig
-  type 'a family
-  val add_element :  'a family -> 'a  -> 'a family
-  val remove_element : 'a family -> 'a -> 'a family
-  val new_family : () -> 'a family
-  exception family_is_empty
-end
-
-module 'a List_family : FAMILY =
-struct
-  type 'a family = 'a list
-  let add_element list elem = elem::list
-                              
-  let new_family () = []
-end
-
 type t = <
   (*TO DO: The type Unit.t list could (should) not be exposed. Instead,
     we could create a module allowing some functions as search one unit,
     get a list of unit etc... *)
   get_army : Unit.t list;
-  set_army : Unit.t list -> unit;
-
   add_unit : Unit.t -> unit;
-  (*remove_unit : Unit.t -> unit;*)
+  set_army : Unit.t list -> unit;
   
   get_buildings : Building.t list;
-  set_buildings : Building.t list -> unit;
-
   add_building : Building.t -> unit;
-  (*remove_building : Building.t -> unit;*)
+  (* set_buildings : Building.t list -> unit; *)
   
-  get_next_action : Action.t;    (* Appelé par la grosse boucle, renvoie depuis la file l'action que veut effectuer le joueur *)
+   get_next_action :  Action.t;    (* Appelé par la grosse boucle, renvoie depuis la file l'action que veut effectuer le joueur *)
 >
 
 val create_player : unit -> t
+
