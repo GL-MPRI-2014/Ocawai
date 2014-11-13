b20238e043ab2494f7904d4e08fd4838fe1579de
tbazin <theis.bazin@ens-cachan.fr>
2014-11-13 12:38:19 +0100

    Added time-constants and time creation functionsO

--- src/music/Time.ml
+++ src/music/Time.ml
@@ -9,9 +9,9 @@ type t = num
 
 let zero = Int(0)
 
-let plus = (+/)
+let plus = ( +/ )
 
-let minus = (-/)
+let minus = ( -/ )
 
 let inverse = minus_num
 
@@ -20,6 +20,33 @@ let compare = compare_num
 let min = min_num
 
 let max = max_num
+
+(** {2 Basic time creation} *)
+
+let fromPair : (int * int) -> t = function
+  | (a, b) -> Num.( // ) (Int a) (Int b)
+
+let bn : t = fromPair (2, 1)
+let wn : t = fromPair (1, 1)
+let hn : t = fromPair (1, 2)
+let qn : t = fromPair (1, 4)
+let en : t = fromPair (1, 8)
+let sn : t = fromPair (1, 16)
+let tn : t = fromPair (1, 32)
+let sfn : t = fromPair (1, 64)
+let dwn : t = fromPair (3, 2)
+let dhn : t = fromPair (3, 4)
+let dqn : t = fromPair (3, 8)
+let den : t = fromPair (3, 16)
+let dsn : t = fromPair (3, 32)
+let dtn : t = fromPair (3, 64)
+let ddhn : t = fromPair (7, 8)
+let ddqn : t = fromPair (7, 16)
+let dden : t = fromPair (7, 32)
+
+(** {2 Testing functions} *)
+
+(** {3 Pretty-printing} *)
 
 let fprintf : Format.formatter -> t -> unit = fun fmt ->
   function
