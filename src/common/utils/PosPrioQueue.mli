c54d9eba6df611303185b2229865f6caaadf2346
dbusatto <damien.busatto@gmail.com>
2014-11-19 23:41:52 +0100

    comments about priority queue

--- src/common/utils/PosPrioQueue.mli
+++ src/common/utils/PosPrioQueue.mli
@@ -1,4 +1,5 @@
-(** Module implementing a modifiable priority queue on positions with an indexed binary heap *)
+(** Module implementing a mutable priority queue on Positions.t with an indexed binary heap 
+    Assume elements unicity, do not add several times the same position*)
 
 (** priority type *)
 type priority = int
@@ -9,21 +10,26 @@ type t
 (** empty queue *)
 val empty : int -> int -> t
 
+(** test if a queue is empty *)
 val is_empty : t -> bool
 
+(** make a queue empty, not reversible
+  (do NOT add positions after calling this method) *)
 val set_empty : t -> unit
 
+(** priority +infinity *)
 val p_none : priority
 
+(** for debug puposes *)
 val print : t -> unit
 
-(** insert an element with a priority in a queue *)
+(** insert an position with a priority in a queue *)
 val push : t -> priority -> Position.t -> unit
 
-(** extract the element with the min priority from a queue *)
+(** extract an element with the minimal priority from a queue *)
 val pop : t -> priority * Position.t
 
-(** returns the element with the min priority from a queue *)
+(** returns an element with the minimal priority from a queue *)
 val top : t -> priority * Position.t
 
 (** decreases the priority of an element *)
