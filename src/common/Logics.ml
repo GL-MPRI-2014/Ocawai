2501b0b051fca9e0a63a62e6536c982ccbd6bc5e
MetaMetaMath <pichu83@hotmail.fr>
2014-11-20 12:39:55 +0100

    Added attack functions in Logics

--- src/common/Logics.ml
+++ src/common/Logics.ml
@@ -154,7 +154,27 @@ let try_next_action player_list player has_played bf order =
       match action with
       | Wait -> (mvt, Wait)
       | End_turn -> failwith "try_next_action: this case is not possible"
+      | Attack_unit (att, def) ->
+	if att <> u then (mvt, Wait) (*only the unit that moved can attack*)
+	else (
+	  let dist = Position.dist (att#position) (def#position) in
+	  let range = (att#min_attack_range, att#attack_range) in
+	  if fst range > dist || snd range < dist then 
+	    (mvt, Wait) (*targeted unit not in range; do nothing*)
+	  else if snd range > 1 && List.length mvt > 1 then
+	    (mvt, Wait) (*a ranged unit must not move before firing*)
+	  else
+	    (mvt, action) (*the attack is valid*)
+	)
       | _ -> (mvt, Wait)
-	(*TODO : manage attacks here*)
+	(*New actions here*)
   )
+
+let apply_attack att def =
+  let lambda = 90 and mu = 10 in
+  let percentage = lambda * att#hp + mu in
+  let div = 100 * 100 * att#life_max in
+  let damage = att#attack def#armor (percentage * 100 / div) in
+  def#take_damage damage
+
 
