20aa7929fa7b7dcfd3d94f6a9c66f7f20e166461
Sheeft <isheeft@gmail.com>
2014-11-27 10:55:15 +0100

    Added static types

--- src/script/Types.mli
+++ src/script/Types.mli
@@ -1,16 +1,16 @@
 (** Basic types for the script parser *)
 
 (** Type of procedures called by the interpreter *)
-type procedure_type = 
+type procedure_type =
     Move   of string list * seq_type
   | Attack of string list * seq_type
   | Main of seq_type
   | Init of seq_type
 
 (** Type of the values manipulated by the AIs *)
-and values_type = 
-    Int     of int 
-  | Unit   
+and values_type =
+    Int     of int
+  | Unit
   | String  of string
   | Bool    of bool
   | List    of values_type list
@@ -18,22 +18,36 @@ and values_type =
   | Var     of string
   | App     of string * values_type list
   | Ifte    of values_type * seq_type * seq_type
+  | Pair    of values_type * values_type
 
 (** Type of a variable/function declaration *)
-and decl_type = 
+and decl_type =
     Vardecl of string * values_type
   | Varset  of string * values_type
   | Fundecl of string * string list * seq_type
 
 (** Type of the sequences (body of a function) *)
-and seq_type = 
+and seq_type =
     Seq     of decl_type * seq_type
   | Return  of values_type
 
 (** Type of a program *)
-and prog_type = 
+and prog_type =
     Globseq of decl_type * prog_type
-  | Procseq of procedure_type * prog_type 
+  | Procseq of procedure_type * prog_type
   | Empty
-    
 
+
+(** Static types *)
+
+(* type typed_ast = prog_type * static *)
+
+type static = [
+  `Int |
+  `Unit |
+  `String |
+  `Bool |
+  `List of static |
+  `Array of static |
+  `Fun of static * static
+]
