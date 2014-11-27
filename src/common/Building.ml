c69f370b112e7ae89b58000ade8080187b856fad
MetaMetaMath <pichu83@hotmail.fr>
2014-11-27 11:19:21 +0100

    Added owner related functions to buildings

--- src/common/Building.ml
+++ src/common/Building.ml
@@ -1,15 +1,18 @@
-class building (p_id : string) (p : Position.t) =
+class building (p : Position.t) =
 object (self)
   val name = ""
   val mutable product = ([] : Unit.t list)
   (* val mutable income = Resource.create 0 *) (* Resource à implanter *)
   val mutable pos = p
+  val mutable player_id = None
   method name = name
-  method id = string_of_int (Oo.id self)
-  method player_id = p_id
+  method id = Oo.id self
+  method player_id = player_id
   method producible = product
   (* method get_income = income *)
   method position = pos
+  method set_owner p_id = player_id <- Some p_id
+  method set_neutral = player_id <- None
 end
 
 type t = building
--- src/common/Building.mli
+++ src/common/Building.mli
@@ -1,23 +1,11 @@
 (** Building interface (draft) *)
 
-(*
-class building :
-object
-  method get_name : string
-  method get_id : string
-  method get_player_id : string
-  method get_producible : Unit.t list
-  (* method get_income : Resource.t *) (* Resource à implanter *)
-  method get_position : Position.t
-end
-
-type t = building
-*)
-
 type t = <
   name : string;
   position : Position.t;
-  id : string;
-  player_id : string;
-  producible : Unit.t list
+  id : int;
+  player_id : int option;
+  producible : Unit.t list;
+  set_owner : int -> unit;
+  set_neutral : unit -> unit
 >
