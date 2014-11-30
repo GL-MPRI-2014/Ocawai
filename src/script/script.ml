8db98539bf21e5843c104381896e017b9c68e214
VLanvin <victor.lanvin@ens-cachan.fr>
2014-11-30 18:03:20 +0100

    Fixed a warning in script.ml

--- src/script/script.ml
+++ src/script/script.ml
@@ -1,3 +1,3 @@
 let () =
-  let scr = ScriptEngine.script_from_file "src/script/test.script" in
-  ignore(scr)
+  let scr = ScriptEngine.script_from_file "src/script/test.script" [] in
+  ignore scr
