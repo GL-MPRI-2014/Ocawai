bf116fd8d32d406e0c15d57def6a41eeb513e5fc
MetaMetaMath <pichu83@hotmail.fr>
2014-12-04 11:54:01 +0100

    Added unbound building and associated functions

--- src/common/Building.ml
+++ src/common/Building.ml
@@ -1,19 +1,53 @@
-class building (p : Position.t) =
+class unbound_building (s : string) (prod : Unit.t list) (inc : int) =
 object (self)
-  val name = ""
-  val mutable product = ([] : Unit.t list)
-  (* val mutable income = Resource.create 0 *) (* Resource à implanter *)
-  val mutable pos = p
-  val mutable player_id = None
+  val name = s
+  val product = prod
+  val income = inc
   method name = name
-  method get_id = Oo.id self
-  method player_id = player_id
   method producible = product
-  (* method get_income = income *)
-  method position = pos
-  method set_owner (p_id : int) = player_id <- Some p_id
-  method set_neutral = player_id <- None
+  method get_income = income
 end
 
+
+class building (ub : unbound_building) (p : Position.t) (p_id : int option) 
+  (id0 : int) =
+object (self)
+  inherit unbound_building ub#s ub#prod ub#inc
+  val position = p
+  val mutable player_id = p_id
+  val mutable id = id0
+  method position = position
+  method get_id = id
+  method player_id = player_id
+  method set_owner (p_id : int) = player_id <- Some p_id
+  method set_neutral = player_id <- None
+
+  initializer if id0 = -1 then id <- Oo.id self
+end
+
+
 type t = building
+type unbound_t = unbound_building
 type id = int
+
+
+let bind ub pos p_id =
+  new building ub pos p_id (-1)
+
+let bind_extended ub pos p_id id =
+  new building ub pos p_id id
+
+
+let create_unbound_from_parsed_building pb =
+  new unbound_building (pb.Building_t.name) (pb.Building_t.product) 
+    (pb.Building_t.get_income)
+
+let create_parsed_unit_from_unbound ub =
+  let open Building_t in
+  {
+    name = ub#name;
+    product = ub#product;
+    get_income = ub#get_income
+  }
+
+
--- src/common/Building.mli
+++ src/common/Building.mli
@@ -2,12 +2,34 @@
 
 
 type id = int
+
+type unbound_t = <
+  name : string;
+  product : Unit.t list;
+  get_income : int
+>
+
 type t = <
   name : string;
   position : Position.t;
   get_id : id;
   player_id : int option;
   producible : Unit.t list;
+  get_income : int;
   set_owner : int -> unit;
   set_neutral : unit
 >
+
+val bind : unbound_t -> Position.t -> int option -> t
+val bind_extended : unbound_t -> Position.t -> int option -> id -> t
+
+val create_unbound_from_parsed_building : Building_t.t -> unbound_t
+val create_parsed_unit_from_unbound : unbound_t -> Building_t.t
+
+
+
+
+
+
+
+
