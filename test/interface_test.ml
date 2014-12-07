2cb6c211f7c5dcee1e6c39eca152306e860b1408
Artymort <m.berta@laposte.net>
2014-12-07 17:37:59 +0100

    Commented interface test, cf #148

--- test/interface_test.ml
+++ test/interface_test.ml
@@ -1,17 +1,17 @@
-open OUnit2
+(*open OUnit2*)
 
-open Manager
+(*open Manager*)
 
-let test_snake test_ctxt =
-  (new Snake.state :> State.state) |> manager#push ;
-  (* Cannot run it if we cannot stop it *)
-  (* manager#run; *)
-  assert_equal () ()
+(*let test_snake test_ctxt =*)
+  (*(new Snake.state :> State.state) |> manager#push ;*)
+   (*Cannot run it if we cannot stop it *)
+   (*manager#run; *)
+  (*assert_equal () ()*)
 
-let suite_interface =
-  "Interface tests" >::: [
-    "Snake testing" >:: test_snake
-  ]
+(*let suite_interface =*)
+  (*"Interface tests" >::: [*)
+    (*"Snake testing" >:: test_snake*)
+  (*]*)
 
-let () =
-  run_test_tt_main suite_interface
+(*let () =*)
+  (*run_test_tt_main suite_interface*)
