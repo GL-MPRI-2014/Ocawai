7923ea03b114f586f5d72c015fe943619e52bc72
Artymort <m.berta@laposte.net>
2014-12-10 11:12:25 +0100

    libxrandr instead of xrandr

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -7,11 +7,12 @@
 OPAM_DEPENDS="ocamlfind ocsfml atdgen mm pulseaudio oUnit dolog"
 LIB_DEPENDS="libboost-all-dev cmake libsfml-dev pulseaudio libpulse-dev"
 COMPILER_DEPENDS="g++ binutils make"
-TESTING_DEPENDS="xdotool xrandr"
+TESTING_DEPENDS="xdotool libxrandr-dev"
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 #cp /etc/machine-id /var/lib/dbus/machine-id
 sudo cp /var/lib/dbus/machine-id /etc/machine-id
+xrandr
 
 case "$OCAML_VERSION" in
 	3.12.1) ppa=avsm/ocaml312+opam12 ;;
