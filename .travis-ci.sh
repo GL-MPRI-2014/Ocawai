9f65369c759169f9e73a575e73de5521b53f6220
Artymort <m.berta@laposte.net>
2014-12-10 11:26:29 +0100

    Tweaking the line ?

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -9,7 +9,7 @@ LIB_DEPENDS="libboost-all-dev cmake libsfml-dev pulseaudio libpulse-dev"
 COMPILER_DEPENDS="g++ binutils make"
 TESTING_DEPENDS="xdotool libxrandr-dev"
 export DISPLAY=:99.0
-sudo /usr/bin/Xvfb +extension RANDR :99 -ac -screen 0 1280x1024x32
+xvfb +extension RANDR :99 -screen 0 1280x1024x32
 #cp /etc/machine-id /var/lib/dbus/machine-id
 sudo cp /var/lib/dbus/machine-id /etc/machine-id
 sudo xrandr
