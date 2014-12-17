22b806bb819c236b58a4a7a21e9ac27dc4d0e9f8
Sheeft <isheeft@gmail.com>
2014-12-17 14:19:07 +0100

    Added attack_range to unit info

--- src/interface/gui/CaseInfo.ml
+++ src/interface/gui/CaseInfo.ml
@@ -10,7 +10,8 @@ let height = 150.
 
 class case_info = object(self)
 
-  method draw : 'a. (#render_target as 'a) -> (string -> (float * float) -> unit) -> Unit.t option -> string -> unit =
+  method draw : 'a. (#render_target as 'a) ->
+  (string -> (float * float) -> unit) -> Unit.t option -> string -> unit =
     fun target drawer u chara ->
       let (w,h) = foi2D target#get_size in
       let x = 10.
@@ -50,7 +51,14 @@ class case_info = object(self)
             (Color.rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left = 125. ; top = h -. 125. ; width = 10. ; height = 50. }
+            { left = 125. ; top = h -. 125. ; width = 10. ; height = 50. };
+          (* Attack Range *)
+          drawer "attack_range" (150.,h-.115.) ;
+          rect_print target
+            ((string_of_int u#min_attack_range)
+              ^ "-" ^ (string_of_int u#attack_range))
+            font (Color.rgb 77 77 77) (Pix 15) (Pix 2) Left
+            { left = 165. ; top = h -. 125. ; width = 30. ; height = 50. }
       | None ->
           rect_print target "No unit there" font (Color.rgb 99 99 99)
             (Pix 15) (Pix 2)
