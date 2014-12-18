da3920b8bcdbb49ce1951e0e61c1aa3d39d9173d
Sheeft <isheeft@gmail.com>
2014-12-18 20:22:31 +0100

    Case info's size is content dependant

--- src/interface/gui/CaseInfo.ml
+++ src/interface/gui/CaseInfo.ml
@@ -17,53 +17,57 @@ class case_info = object(self)
       let (w,h) = foi2D target#get_size in
       let x = 10.
       and y = h -. 160. in
+      let uh_offset = if u = None
+        then 50.
+        else 0.
+      in
+      let bh_offset = if building = None
+        then 50.
+        else 0.
+      in
       new rectangle_shape
-        ~position:(x,y)
-        ~size:(width,height)
+        ~position:(x,y+.uh_offset+.bh_offset)
+        ~size:(width,height-.uh_offset-.bh_offset)
         ~fill_color:(Color.rgba 210 230 255 240)
         ()
       |> target#draw ;
       (* Unit information *)
       begin match u with
       | Some u ->
-          drawer (chara ^ "_" ^ u#name) (30.,h-.140.) ;
+          drawer (chara ^ "_" ^ u#name) (30.,h-.140.+.bh_offset) ;
           (* Name *)
           rect_print target u#name font (Color.rgb 33 33 33) (Pix 15) (Pix 2)
             Left
-            { left = 50. ; top = h -. 150. ; width = 170. ; height = 50. };
+            { left=50. ; top=h-.150.+.bh_offset ; width=170. ; height=50. };
           (* Player ID *)
           rect_print target ("#" ^ (string_of_int u#player_id)) font
             (Color.rgb 77 77 77)
             (Pix 15) (Pix 2)
             Right
-            { left = 50. ; top = h -. 150. ; width = 170. ; height = 50. } ;
+            { left=50. ; top=h-.150.+.bh_offset ; width=170. ; height=50. } ;
           (* Life *)
-          drawer "life" (25.,h-.115.) ;
+          drawer "life" (25.,h-.115.+.bh_offset) ;
           rect_print target
             ((string_of_int u#hp) ^ "/" ^ (string_of_int u#life_max)) font
             (Color.rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left = 37. ; top = h -. 125. ; width = 60. ; height = 50. };
+            { left=37. ; top=h-.125.+.bh_offset ; width=60. ; height=50. };
           (* Move Range *)
-          drawer "move_range" (110.,h-.115.) ;
+          drawer "move_range" (110.,h-.115.+.bh_offset) ;
           rect_print target (string_of_int u#move_range) font
             (Color.rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left = 125. ; top = h -. 125. ; width = 10. ; height = 50. };
+            { left=125. ; top=h-.125.+.bh_offset ; width=10. ; height=50. };
           (* Attack Range *)
-          drawer "attack_range" (150.,h-.115.) ;
+          drawer "attack_range" (150.,h-.115.+.bh_offset) ;
           rect_print target
             ((string_of_int u#min_attack_range)
               ^ "-" ^ (string_of_int u#attack_range))
             font (Color.rgb 77 77 77) (Pix 15) (Pix 2) Left
-            { left = 165. ; top = h -. 125. ; width = 30. ; height = 50. }
-      | None ->
-          rect_print target "No unit there" font (Color.rgb 99 99 99)
-            (Pix 15) (Pix 2)
-            Center
-            { left = 30. ; top = h -. 150. ; width = 170. ; height = 50. }
+            { left=165. ; top=h-.125.+.bh_offset ; width=30. ; height=50. }
+      | None -> ()
       end ;
       (* Building information *)
       begin match building with
@@ -92,11 +96,7 @@ class case_info = object(self)
             (Pix 15) (Pix 2)
             Left
             { left = 37. ; top = h -. 70. ; width = 60. ; height = 50. }
-      | None ->
-          rect_print target "No building there" font (Color.rgb 99 99 99)
-            (Pix 15) (Pix 2)
-            Center
-            { left = 30. ; top = h -. 95. ; width = 170. ; height = 50. }
+      | None -> ()
       end ;
       (* Tile information *)
       tile_drawer (Tile.get_name tile) (20.,h-.50.) ;
