a3bc0ad76730c254d496b510ad79b1627d91004e
MetaMetaMath <pichu83@hotmail.fr>
2014-12-18 10:54:41 +0100

    Improved doc for Logics

--- src/common/Logics.mli
+++ src/common/Logics.mli
@@ -1,19 +1,20 @@
 (** Some useful functions related to the game's logic *)
 type accessibles = Position.t list * (Position.t, Path.t) Hashtbl.t
 
-(** Returns the list of positions that a given unit can see *)
+(** @return the list of positions that a given unit can see *)
 val unit_vision : Unit.t -> Battlefield.t -> Position.t list
 
-(** Returns the list of all positions that a given player can see *)
+(** @return the list of all positions that a given player can see *)
 val player_vision : Player.logicPlayer -> Battlefield.t -> Position.t list
 
-(** Return true iff there is an unit on position [pos]*)
+(** [is_unit_on pos player_list] returns true iff there is an unit on 
+    position [pos]*)
 val is_unit_on : Position.t -> Player.logicPlayer list -> bool
 
 (** For a given unit, returns the list of accessible positions and the
     hashtable where the keys are the accessible positions and the entry
     is a shortest path to this position *)
-val accessible_positions : Unit.t -> Player.logicPlayer -> Player.logicPlayer list -> Battlefield.t -> Position.t list * (Position.t, Path.t) Hashtbl.t
+val accessible_positions : Unit.t -> Player.logicPlayer -> Player.logicPlayer list -> Battlefield.t -> accessibles
 
 (** Same as accessible_positions, but without positions where an allied unit
     is already stationned. *)
@@ -26,23 +27,24 @@ val available_positions : Unit.t -> Player.logicPlayer ->
 val try_next_action : Player.logicPlayer list -> Player.logicPlayer ->
   Battlefield.t -> Action.t -> Action.t
 
-(** Returns the unit of the given player at the given position.
+(** [find_unit pos player] returns the unit of [player] at position [pos].
     Raises Bad_unit if it could not be found. *)
 val find_unit : Position.t -> Player.logicPlayer -> Unit.t
 
-(** Find the player with given id from the list of players *)
+(** [find_player id player_list] returns the player with id [id] *)
 val find_player : int -> Player.logicPlayer list -> Player.logicPlayer
 
-(** Computes and apply the damage of an attack. *)
+(** [apply_attack att def] computes and apply the damage of an attack of [att]
+    against [def]. *)
 val apply_attack : Unit.t -> Unit.t -> unit
 
 (** Computes owner changes for buildings at the start of a turn *)
 val capture_buildings : Player.logicPlayer list -> Player.logicPlayer ->
   Building.t list -> (Building.t * (Player.logicPlayer option)) list
 
-(** Returns the minimum and maximum damage for an attack *)
+(** @return the minimum and maximum damage for an attack *)
 val damage_interval : Unit.t -> Unit.t -> int * int
 
-(** Returns the list of the ennemy units in range *)
+(** @return the list of the ennemy units in range *)
 val units_inrange : Position.t -> int -> Player.logicPlayer ->
   Player.logicPlayer list -> Unit.t list
