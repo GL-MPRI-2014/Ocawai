f28dfe7f35bff1114636a4d4f2d9d68adc102e40
Sheeft <isheeft@gmail.com>
2014-12-18 14:58:26 +0100

    Various fixes (to content Travis)

--- src/common/ClientPlayer.ml
+++ src/common/ClientPlayer.ml
@@ -3,34 +3,34 @@ open Player
 type action_state = Idle | Waiting | Received of Action.t
 
 
-class client_player ?(id) add_update = 
+class client_player ?(id) add_update =
 
 
-  object (self) 
+  object (self)
 
-  inherit player ?id:id
+  inherit player ?id:id ()
 
   val mutable event_state = Idle
 
-  val mutable logicPlayerList = [] 
+  val mutable logicPlayerList = []
 
   method event_state = event_state
 
   method set_state s = event_state <- s
 
-  method get_next_action = 
+  method get_next_action =
     event_state <- Waiting;
-    let rec get_aux () = 
+    let rec get_aux () =
       Thread.delay 0.25;
       match event_state with
-      |Received(a) -> 
+      |Received(a) ->
           event_state <- Idle; a
       | _ -> get_aux ()
     in get_aux ()
 
 
   method update (u:Types.update) = add_update u
-    
+
 
 
 end
