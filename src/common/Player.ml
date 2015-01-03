a6780c8610a38da3ee47ce7309664eec63eeb2dc
Sheeft <isheeft@gmail.com>
2015-01-03 12:02:04 +0100

    Changed for replace in hashtbls
    As suggested by @Vlanvin in #173

--- src/common/Player.ml
+++ src/common/Player.ml
@@ -26,7 +26,8 @@ class logicPlayer ?id
       | Some(id__) -> id__
 
     method has_playable_unit =
-      List.fold_left (fun b u -> b || (not u#has_played)) false (self#get_army)
+      (* List.fold_left (fun b u -> b || (not u#has_played)) false (self#get_army) *)
+      List.exists (fun u -> not u#has_played) self#get_army
 
     method get_army =
       Hashtbl.fold (fun id u l -> u::l) army []
@@ -48,12 +49,14 @@ class logicPlayer ?id
     method get_fog = fog
 
     method set_army a =
-      List.iter (fun unit -> self#add_unit unit) a;
-      if Array.length fog > 0 then List.iter (fun x -> Fog.add_unit_fog fog x#position x#vision_range) a
+      List.iter (fun u -> self#add_unit u) a;
+      if Array.length fog > 0 then
+        List.iter (fun x -> Fog.add_unit_fog fog x#position x#vision_range) a
 
     method add_unit u =
-        Hashtbl.add army u#get_id u;
-        if Array.length fog > 0 then Fog.add_unit_fog fog u#position u#vision_range
+        Hashtbl.replace army u#get_id u;
+        if Array.length fog > 0 then
+          Fog.add_unit_fog fog u#position u#vision_range
         (*TODO*)
 
     method set_buildings b =
@@ -61,49 +64,69 @@ class logicPlayer ?id
 
     method get_buildings =
       Hashtbl.fold (fun id b l -> b::l) buildings []
+
     method get_base = base
+
     method set_base b = base <- Some b
 
     method add_building (b:Building.t) =
-        Hashtbl.add buildings b#get_id b;
-        if Array.length fog > 0 then Fog.add_unit_fog fog b#position b#vision_range
+        Hashtbl.replace buildings b#get_id b;
+        if Array.length fog > 0 then
+          Fog.add_unit_fog fog b#position b#vision_range
 
     (* TODO *)
     method set_unit_hp (u : Unit.id) (h : int) = ()
 
     method delete_unit (id_unit : Unit.id) =
       let u = (Hashtbl.find army id_unit) in
-      if Array.length fog > 0 then Fog.delete_unit_fog fog u#position u#vision_range;
+      if Array.length fog > 0 then
+        Fog.delete_unit_fog fog u#position u#vision_range;
       Hashtbl.remove army id_unit
 
 
-    method get_unit_by_id (id_unit : Unit.id) = Hashtbl.find army id_unit
-    method get_building_by_id (id_building : Building.id) = Hashtbl.find buildings id_building
+    method get_unit_by_id (id_unit : Unit.id) =
+      Hashtbl.find army id_unit
+
+    method get_building_by_id (id_building : Building.id) =
+      Hashtbl.find buildings id_building
 
     (*it is quite dirty*)
     method move_unit (id_unit : Unit.id) (p : Action.movement) =
       let u = self#get_unit_by_id id_unit in
-      if Array.length fog > 0 then Fog.delete_unit_fog fog u#position u#vision_range;
+      if Array.length fog > 0 then
+        Fog.delete_unit_fog fog u#position u#vision_range;
       u#move (final_position (get_path p));
-      if Array.length fog > 0 then Fog.add_unit_fog fog u#position u#vision_range
+      if Array.length fog > 0 then
+        Fog.add_unit_fog fog u#position u#vision_range
 
     method delete_building (id_building : Building.id) =
         let b = Hashtbl.find buildings id_building in
-        if Array.length fog > 0 then Fog.delete_unit_fog fog b#position b#vision_range;
+        if Array.length fog > 0 then
+          Fog.delete_unit_fog fog b#position b#vision_range;
         Hashtbl.remove buildings id_building
 
     method get_value_resource = resource
 
-    method has_resource amount = if resource < amount then false else true
+    method has_resource amount =
+      if resource < amount then false else true
 
-    method use_resource amount = if resource < amount then raise Not_enough_ressource else ( resource <- resource - amount)
+    method use_resource amount =
+      if resource < amount then
+        raise Not_enough_ressource
+      else
+        resource <- resource - amount
 
-    method harvest_buildings_income = List.iter (fun b -> resource <- max 0 (resource + b#income)) self#get_buildings
+    method harvest_buildings_income =
+      List.iter
+        (fun b -> resource <- max 0 (resource + b#income))
+        self#get_buildings
 
     method init (field: Battlefield.t) (players:logicPlayer list) =
         let (size_x,size_y) = Battlefield.size field in
         fog <- Array.make_matrix size_x size_y 0;
-        List.iter (fun x -> Fog.add_unit_fog fog x#position x#vision_range) self#get_army;
+        List.iter
+          (fun x -> Fog.add_unit_fog fog x#position x#vision_range)
+          self#get_army
 
     method copy =
       let copy_hashtbl h =
