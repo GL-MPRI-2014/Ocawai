7ea8b616b7bfa5b400423d0f8544ea382ee094bb
Olivier Marty <olivier.marty.m@gmail.com>
2015-01-04 22:20:00 +0100

    End of test_script

--- test/script_test.ml
+++ test/script_test.ml
@@ -1,9 +1,25 @@
 open OUnit2
+open Lexing
+open Checker
 
 (* check assoc *)
 let test_assoc test_ctxt =
-  let script = ScriptEngine.script_from_file "script_test_assoc.script" [] [] in
-  assert_equal (Interpreter.call_f script "test_assoc") (`Bool(true))
+  ScriptCore.init ();
+  let f = "test/script_test_assoc.script" in
+  let input = open_in f in
+  let lexbuf = from_channel input in
+  lexbuf.lex_curr_p <- {lexbuf.lex_curr_p with pos_fname = f};
+  try
+    let script = Parser.file Lexer.token lexbuf in
+    close_in input;
+    type_check script [];
+    let script = Interpreter.new_script script [] in
+    assert_equal (Interpreter.call_f script "test_assoc") (`Bool(true))
+  with
+    |Lexer.Script_SyntaxError _ ->
+        assert_failure "lexing error"
+    |Parser.Error ->
+        assert_failure "parsing error"
 
 let suite_script =
   "script test">:::
