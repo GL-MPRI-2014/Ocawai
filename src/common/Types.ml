90e12a0ac72c3e77b4c1e472c4e2f7d6a7d01a31
Julien Grange <julien.grange@ens-cachan.fr>
2015-01-08 20:15:20 +0100

    Changed special characters in network protocol, and managed Map

--- src/common/Types.ml
+++ src/common/Types.ml
@@ -60,166 +60,182 @@ let disassemble str sep =
 
 
 
-(* ALL THE BLACKOUT METHODS NEED a Config.config OBJECT, WHICH DOESN'T EXIST YET IN DEALER AND NETPLAYER... *)
+let sep1 = '\000'
+and sep2 = '\001'
 
 let from_string (str : string) =
 
-  let lst = disassemble str '@' in
-  match lst with
-  | [] -> failwith "Error in protocol"
-  | t::q -> match t with
+  let length = String.length str in
 
-	    | "Game_over" -> Game_over
+  (* exeption : ma_to_string(map) can contain any character *)
+  
 
-	    | "Your_turn" -> Your_turn
+  if (length >= 3 && String.compare (String.sub str 0 3) "Map" == 0)
+  then Map (String.sub str 3 (length - 3))
+  else
+    (* Not a map *)
 
-	    | "Turn_of" -> begin
-			   match q with
-			   | id_str::_ -> Turn_of (int_of_string id_str)
-			   | _ -> failwith "Error in potocol"
-			 end
+    let lst = disassemble str sep1 in
+    match lst with
+      
+    | [] -> failwith "Error in protocol"
 
-	    | "Classement" -> Classement
+    | t::q -> match t with
 
-	    | "Harvest_income" -> Harvest_income
+	      | "Game_over" -> Game_over
 
-	    (*| "Set_army" -> begin
-			    match q with
-			    | army_str::id_str::_ -> let unit_str_lst = disassemble str '&' in
-						     Set_army (List.map config#unit_of_string unit_str_lst, int_of_string id_str)
-			    | _ -> failwith "Error in protocol"
-			  end *)
+	      | "Your_turn" -> Your_turn
 
-	    (*| "Set_building" -> begin
-				match q with
-				| buildings_str::id_str::_ = q -> let building_str_lst = disassemble str '&' in
-								  Set_building (List.map list config#building_of_string, int_of_string id)
-				| _ -> failwith "Error in protocol"
-			      end *)
-
-	    (*| "Add_unit" -> begin
-			    match q with
-			    | unit_str::id_str::_ -> Add_unit (config#unit_of_string unit_str, int_of_string id_str)
-			    | _ -> failwith "Error in protocol"
-			  end *)
-
-	    (*| "Add_building" -> begin
-				match q with
-				| building_str::id_str::_ -> Add_unit (config#building_of_string building_str, int_of_string id_str)
-				| _ -> failwith "Error in protocol"
-			      end *)
-
-
-	    | "Delete_unit" -> begin
-			       match q with
-			       | unit_id_str::id_str::_ -> Delete_unit (int_of_string unit_id_str, int_of_string id_str)
-			       | _ -> failwith "Error in protocol"
-			     end
-
-
-	    | "Delete_building" -> begin
-				   match q with
-				   | building_id_str::id_str::_ -> Delete_building (int_of_string building_id_str, int_of_string id_str)
-				   | _ -> failwith "Error in protocol"
-				 end
-
-
-	    | "Move_unit" -> begin
+	      | "Turn_of" -> begin
 			     match q with
-			     | unit_id_str::mov_str::id_str::_ -> Move_unit (int_of_string unit_id_str, Action.mov_from_string mov_str, int_of_string id_str)
-			     | _ -> failwith "Error in protocol"
+			     | id_str::_ -> Turn_of (int_of_string id_str)
+			     | _ -> failwith "Error in potocol"
 			   end
 
+	      | "Classement" -> Classement
 
-	    | "Set_unit_hp" -> begin
-			       match q with
-			       | unit_id_str::int_str::id_str::_ -> Set_unit_hp (int_of_string unit_id_str, int_of_string int_str, int_of_string id_str)
-			       | _ -> failwith "Error in protocol"
-			     end
+	      | "You_win" -> You_win
 
-	    | "Set_client_player" -> begin
+	      | "Harvest_income" -> Harvest_income
+
+	      | "Set_army" -> begin
+			      match q with
+			      | army_str::id_str::_ -> let unit_str_lst = disassemble str sep2 in
+						       Set_army (List.map Config.config#unit_of_string unit_str_lst, int_of_string id_str)
+			      | _ -> failwith "Error in protocol"
+			    end
+
+	      | "Set_building" -> begin
+				  match q with
+				  | buildings_str::id_str::_ -> let building_str_lst = disassemble str sep2 in
+								Set_building (List.map Config.config#building_of_string building_str_lst, int_of_string id_str)
+				  | _ -> failwith "Error in protocol"
+				end
+
+	      | "Add_unit" -> begin
+			      match q with
+			      | unit_str::id_str::_ -> Add_unit (Config.config#unit_of_string unit_str, int_of_string id_str)
+			      | _ -> failwith "Error in protocol"
+			    end
+
+	      | "Add_building" -> begin
+				  match q with
+				  | building_str::id_str::_ -> Add_building (Config.config#building_of_string building_str, int_of_string id_str)
+				  | _ -> failwith "Error in protocol"
+				end
+
+
+	      | "Delete_unit" -> begin
+				 match q with
+				 | unit_id_str::id_str::_ -> Delete_unit (int_of_string unit_id_str, int_of_string id_str)
+				 | _ -> failwith "Error in protocol"
+			       end
+
+
+	      | "Delete_building" -> begin
 				     match q with
-				     | id_str::_ -> Set_client_player (int_of_string id_str)
+				     | building_id_str::id_str::_ -> Delete_building (int_of_string building_id_str, int_of_string id_str)
 				     | _ -> failwith "Error in protocol"
 				   end
 
 
-	    | "Set_logic_player_list" -> begin
-					 match q with
-					 | id_lst_str::_ -> let id_str_lst = disassemble id_lst_str '&' in
-							    Set_logic_player_list (List.map int_of_string id_str_lst)
-					 | _ -> failwith "Error in protocol"
-				       end
-
-	    | "Map" -> begin
-		       match q with
-		       | string::_ -> Map string
-		       | _ -> failwith "Error in protocol"
-		     end
-
-	    (*| "Building_changed" -> begin
-				    match q with
-				    | building_str::_ -> Building_changed (config#building_of_string building_str)
-				    | _ -> failwith "Error in protocol"
-				  end *)
-
-	    | _ -> failwith "Error in protocol"
+	      | "Move_unit" -> begin
+			       match q with
+			       | unit_id_str::mov_str::id_str::_ -> Move_unit (int_of_string unit_id_str, Action.mov_from_string mov_str, int_of_string id_str)
+			       | _ -> failwith "Error in protocol"
+			     end
 
 
+	      | "Set_unit_hp" -> begin
+				 match q with
+				 | unit_id_str::int_str::id_str::_ -> Set_unit_hp (int_of_string unit_id_str, int_of_string int_str, int_of_string id_str)
+				 | _ -> failwith "Error in protocol"
+			       end
 
-let to_string = function
+	      | "Set_client_player" -> begin
+				       match q with
+				       | id_str::_ -> Set_client_player (int_of_string id_str)
+				       | _ -> failwith "Error in protocol"
+				     end
+
+
+	      | "Set_logic_player_list" -> begin
+					   match q with
+					   | id_lst_str::_ -> let id_str_lst = disassemble id_lst_str sep2 in
+							      Set_logic_player_list (List.map int_of_string id_str_lst)
+					   | _ -> failwith "Error in protocol"
+					 end
+
+	      | "Building_changed" -> begin
+				      match q with
+				      | building_str::_ -> Building_changed (Config.config#building_of_string building_str)
+				      | _ -> failwith "Error in protocol"
+				    end
+
+	      | _ -> failwith "Error in protocol"
+
+
+
+let to_string update = 
+  let str1 = String.make 1 sep1
+  and str2 = String.make 1 sep2 in
+  match update with
+
   | Game_over ->
      "Game_over"
-
+       
   | Your_turn ->
      "Your_turn"
 
   | Turn_of id_player ->
-     String.concat "@" ("Turn_of"::(string_of_int id_player)::[])
+     String.concat str1 ("Turn_of"::(string_of_int id_player)::[])
 
   | Classement ->
      "Classement"
 
+  | You_win ->
+     "You_win"
+
   | Harvest_income ->
      "Harvest_income"
 
-  (*| Set_army (unit_list, id_player) ->
-     let str = String.concat "&" (List.map config#string_of_unit unit_list) in
-     String.concat "@" ("Set_army"::str::(string_of_int id_player)::[]) *)
+  | Set_army (unit_list, id_player) ->
+     let str = String.concat str2 (List.map Config.config#string_of_unit unit_list) in
+     String.concat str1 ("Set_army"::str::(string_of_int id_player)::[])
 
-  (*| Set_building (building_list, id_player) ->
-     let str = String.concat "&" (List.map config#string_of_building building_list) in
-     String.concat "@" ("Set_building"::str::(string_of_int id_player)::[]) *)
+  | Set_building (building_list, id_player) ->
+     let str = String.concat str2 (List.map Config.config#string_of_building building_list) in
+     String.concat str1 ("Set_building"::str::(string_of_int id_player)::[])
 
-  (*| Add_unit (unit, id_player) ->
-     String.concat "@" ("Add_unit"::(config#string_of_unit unit)::(string_of_int id_player)::[]) *)
+  | Add_unit (unit, id_player) ->
+     String.concat str1 ("Add_unit"::(Config.config#string_of_unit unit)::(string_of_int id_player)::[])
 
-  (*| Add_building (building, id_player) ->
-       String.concat "@" ("Add_building"::(config#string_of_building building)::(string_of_int id_player)::[]) *)
+  | Add_building (building, id_player) ->
+       String.concat str1 ("Add_building"::(Config.config#string_of_building building)::(string_of_int id_player)::[])
 
   | Delete_unit (id_unit, id_player) ->
-     String.concat "@" ("Delete_unit"::(string_of_int id_unit)::(string_of_int id_player)::[])
+     String.concat str1 ("Delete_unit"::(string_of_int id_unit)::(string_of_int id_player)::[])
 
   | Delete_building (id_building, id_player) ->
-     String.concat "@" ("Delete_building"::(string_of_int id_building)::(string_of_int id_player)::[])
+     String.concat str1 ("Delete_building"::(string_of_int id_building)::(string_of_int id_player)::[])
 
   | Move_unit (id_unit, movement, id_player) ->
-     String.concat "@" ("Move_unit"::(string_of_int id_unit)::(Action.mov_to_string movement)::(string_of_int id_player)::[])
-
+     String.concat str1 ("Move_unit"::(string_of_int id_unit)::(Action.mov_to_string movement)::(string_of_int id_player)::[])
+		   
   | Set_unit_hp (id_unit, int, id_player) ->
-     String.concat "@" ("Set_unit_hp"::(string_of_int id_unit)::(string_of_int int)::(string_of_int id_player)::[])
-
+     String.concat str1 ("Set_unit_hp"::(string_of_int id_unit)::(string_of_int int)::(string_of_int id_player)::[])
+		   
   | Set_client_player id_player ->
-     String.concat "@" ("Set_client_player"::(string_of_int id_player)::[])
-
+     String.concat str1 ("Set_client_player"::(string_of_int id_player)::[])
+		   
   | Set_logic_player_list id_player_list ->
-     let str = String.concat "&" (List.map string_of_int id_player_list) in
-     String.concat "@" ("Set_logic_player_list"::str::[])
-
+     let str = String.concat str2 (List.map string_of_int id_player_list) in
+     String.concat str1 ("Set_logic_player_list"::str::[])
+		   
   | Map string ->
-     String.concat "@" ("Map"::string::[])
+     String.concat "" ("Map"::string::[])
 
-  (*| Building_changed building ->
-     String.concat "@" (config#string_of_building building::[]) *)
+  | Building_changed building ->
+     String.concat str1 (Config.config#string_of_building building::[])
 
   | _ -> failwith "Update not known"
